7c5f1d68c3e029352dac50c73f435e4738577e0a
Ace Alrich <hex0@live.com>
2021-05-02 22:03:52 +0100

    gsap scroll

--- scripts.js
+++ scripts.js
@@ -1,4 +1,24 @@
-gsap.to(".box", {
-  scrollTrigger: ".max-stats-section",
-  x: 500,
+gsap.registerPlugin(ScrollTrigger);
+function goToSection(i, anim) {
+  gsap.to(window, {
+    scrollTo: { y: i * innerHeight, autoKill: false },
+    duration: 0.5,
+  });
+
+  if (anim) {
+    anim.restart();
+  }
+}
+
+gsap.utils.toArray(".panel").forEach((panel, i) => {
+  ScrollTrigger.create({
+    trigger: panel,
+    onEnter: () => goToSection(i),
+  });
+
+  ScrollTrigger.create({
+    trigger: panel,
+    start: "bottom bottom",
+    onEnterBack: () => goToSection(i),
+  });
 });
