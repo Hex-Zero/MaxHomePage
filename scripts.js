463cfd45af32a451611d8e030d53c2c5f2993fbc
Ace Alrich <hex0@live.com>
2021-06-06 01:04:31 +0100

    status template

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
+var $statusComponent = $("#statusComponentTemplate").detach();
 
+function createStatus(component, text) {
+  var $status = component.clone();
+  return $status.html(text);
+}
+
+for (i = 0; i < 4; i++) {
+  $(".max-stats-section").append(createStatus($statusComponent, "status " + i));
+}
